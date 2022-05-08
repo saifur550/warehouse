93fd22b937a03018f67b81e297732616107e7455
saifur550 <srstap550@gmail.com>
2022-05-09 03:45:11 +0600

    question part one

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,8 +5,29 @@ const About = () => {
         <div className='container'>
             <div className="row">
                 <div className="col-lg-10 mx-auto">
-                    <div className="part-one">
-                        <p>Difference between `javascript` and `nodejs`</p>
+                    <div className="part-one mt-3">
+                        <h4>1. Difference between `javascript` and `nodejs`</h4>
+                        <h5>Javascript</h5>
+                        <ul>
+                            <li>Javascript is a lightweight, object-oriented scripting language that is used to build dynamic HTML pages with interactive effects on a webpage. JavaScript is also commonly used in game development and mobile app development.</li>
+
+                            <li>It is an open-source, flexible, fast, light-weighted framework.</li>
+                            <li>It allows cross-compilation.</li>
+                            <li>It supports interfaces, modules, and classes.</li>
+                            <li>We may extend the JavaScript for writing large applications.</li>
+                        </ul>
+
+                        <h5>Node Js</h5>
+
+                        <ul>
+                            <li>Node.js is a cross-platform, open-source JavaScript runtime environment that enables JavaScript to be run on the server. Node.js enables JavaScript code to run outside of the browser.</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+
                     </div>
                 </div>
             </div>
